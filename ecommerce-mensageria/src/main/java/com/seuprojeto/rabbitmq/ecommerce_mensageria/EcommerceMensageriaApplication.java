14edb70f1f9546df191eb4010f1f7135403f862e
Guilherme Pereira <gfranciscosilvapereira@gmail.com>
2025-10-07 11:19:12 -0300

    ad

---
+++ ecommerce-mensageria/src/main/java/com/seuprojeto/rabbitmq/ecommerce_mensageria/EcommerceMensageriaApplication.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.rabbitmq.ecommerce_mensageria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMensageriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMensageriaApplication.class, args);
+	}
+
+}
---
+++ ecommerce-mensageria/src/test/java/com/seuprojeto/rabbitmq/ecommerce_mensageria/EcommerceMensageriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.rabbitmq.ecommerce_mensageria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMensageriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
